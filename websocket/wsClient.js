4fb126bec4f718419b35f903785dea047e423607
atouloupis <atouloupis@yahoo.Fr>
2017-11-17 10:55:45 +0100

    Add files via upload

--- websocket/wsClient.js
+++ websocket/wsClient.js
@@ -16,10 +16,10 @@ client.on('connect', function(connection) {
         console.log('echo-protocol Connection Closed');
     });
     connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message + "'");
+        //if (message.type === 'utf8') {
+            console.log("Received: '" + JSON.stringify(message) + "'");
 			//return JSON.stringify(message);;
-        }
+        //}
     });
     function sendRequest() {
         if (connection.connected) {
