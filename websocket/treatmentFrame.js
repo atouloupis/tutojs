c41d307baebcaefd8709ff8d1f9aab0c1962657b
atouloupis <atouloupis@yahoo.Fr>
2017-11-22 16:42:54 +0100

    Add files via upload

--- websocket/treatmentFrame.js
+++ websocket/treatmentFrame.js
@@ -10,7 +10,8 @@ console.log(JSON.stringify(jsonFrame));
 	}
 	if (jsonFrame.method = "activeOrders")
 	{
-		var foreachframe = jsonFrame.params[0];
+		console.log(JSON.stringify(jsonFrame));
+		var foreachframe = jsonFrame.params;
 		console.log(JSON.stringify(foreachframe));
 		if (foreachframe =! "undefined")
 			{
