8ba92a0136f308871ee21063c20398758b7620df
atouloupis <atouloupis@yahoo.Fr>
2017-11-22 17:14:01 +0100

    Add files via upload

--- websocket/treatmentFrame.js
+++ websocket/treatmentFrame.js
@@ -2,6 +2,7 @@ module.exports.splitFrame = splitFrame;
 
 
 function splitFrame (frame){
+console.log("#"+JSON.stringify(Frame));
 	var jsonFrame = JSON.parse(frame);
 console.log("##"+JSON.stringify(jsonFrame));
 	if (jsonFrame.method = "ticker")
@@ -10,14 +11,14 @@ console.log("##"+JSON.stringify(jsonFrame));
 	}
 	if (jsonFrame.method = "activeOrders")
 	{
-		console.log("###"+JSON.stringify(jsonFrame));
+		//console.log("###"+JSON.stringify(jsonFrame));
 		var foreachframe = jsonFrame.params;
-		console.log("####"+JSON.stringify(foreachframe));
+		//console.log("####"+JSON.stringify(foreachframe));
 		if (foreachframe =! "undefined")
 			{
 			console.log(JSON.stringify(jsonFrame));
 			foreachframe.forEach(function (orders){
-				console.log(orders.symbol);
+				//console.log(orders.symbol);
 				});
 			}
 	}
