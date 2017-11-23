990376ab02369853db2883760907670a564e3c49
atouloupis <atouloupis@yahoo.Fr>
2017-11-23 17:18:28 +0100

    Add files via upload

--- batch_activeorder.sh
+++ batch_activeorder.sh
@@ -1,1 +1,1 @@
-curl -u "75ea4dceeb285ee86c026d62700df14f:919c86f1a996fa98ba4fc74ebb1a364d" \ "https://api.hitbtc.com/api/2/order" -X GET > websocket/activeOrders.json
+curl -X GET -u "75ea4dceeb285ee86c026d62700df14f:919c86f1a996fa98ba4fc74ebb1a364d" : "https://api.hitbtc.com/api/2/order" > websocket/activeOrders.json
