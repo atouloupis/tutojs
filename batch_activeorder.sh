429cf9ece94a3113afccdc8f4578beab4b14fb3e
atouloupis <atouloupis@yahoo.Fr>
2017-11-24 16:43:18 +0100

    Add files via upload

--- batch_activeorder.sh
+++ batch_activeorder.sh
@@ -1,2 +1,2 @@
 curl -X GET https://api.hitbtc.com/api/2/trading/balance -u 75ea4dceeb285ee86c026d62700df14f:919c86
-f1a996fa98ba4fc74ebb1a364d
+f1a996fa98ba4fc74ebb1a364d > ./data/activeOrders.json
