a598e72f24a421f2972f4a47229d20bf848f7414
atouloupis <atouloupis@yahoo.Fr>
2017-11-24 17:22:35 +0100

    Add files via upload

--- websocket/updateActiveOrders.js
+++ websocket/updateActiveOrders.js
@@ -9,7 +9,7 @@ console.log(JSON.stringify(activeOrders));
 	for (var i=0; i < frame.length; i++){
 		for (var j=0;j<activeOrders.length;j++)
 		{
-			if (frame[i].symbol == activeOders[j].symbol) console.log(JSON.stringify(activeOders[j].symbol));
+			if (frame[i].symbol == activeOrders[j].symbol) console.log(JSON.stringify(activeOrders[j].symbol));
 		}		
 	}
 
