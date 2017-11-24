eec39aea55522c3d66f3c3cbc6bcba2c35772e30
atouloupis <atouloupis@yahoo.Fr>
2017-11-24 16:59:46 +0100

    Add files via upload

--- websocket/getWes.js
+++ websocket/getWes.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
 
-var WebSocketClient = require('websocket').client;
-var jsonfile = require('jsonfile');
-var treatment = require('./treatmentFrame');
-var updtOrders = require ('./updateActiveOrders');
 var wsClient = require ('./wsClient');
