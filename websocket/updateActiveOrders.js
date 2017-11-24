bfe88b40a6f4d9cd8419c9522c8aeef1110162df
atouloupis <atouloupis@yahoo.Fr>
2017-11-24 17:05:50 +0100

    Add files via upload

--- websocket/updateActiveOrders.js
+++ websocket/updateActiveOrders.js
@@ -4,7 +4,7 @@ module.exports.newActiveOrders = newActiveOrders;
 function newActiveOrders (frame){
 
 var activeOrders = require('../data/activeOrders.json');
-console.log(JSON.stringify(activeOrder)); 
+console.log(JSON.stringify(activeOrders)); 
 //if (frame.clientOrderId in 
 
 };
