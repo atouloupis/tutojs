2a6ffd485e58cf4ee8f50b22d9f06f5d76976191
atouloupis <atouloupis@yahoo.Fr>
2017-11-28 18:19:25 +0100

    Add files via upload

--- websocket/wsClient.js
+++ websocket/wsClient.js
@@ -12,34 +12,68 @@ var file = 'data.json';
 
 var client = new WebSocketClient();
  
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
-});
+// client.on('connectFailed', function(error) {
+    // console.log('Connect Error: ' + error.toString());
+// });
  
-client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', function (message) { 
+// client.on('connect', function(connection) {
+    // console.log('WebSocket Client Connected');
+    // connection.on('error', function(error) {
+        // console.log("Connection Error: " + error.toString());
+    // });
+    // connection.on('close', function() {
+        // console.log('echo-protocol Connection Closed');
+    // });
+    // connection.on('message', function (message) { 
 		// console.log(message);
-		var utf8message=message.utf8Data; 
-		console.log(utf8message);
+		// var utf8message=message.utf8Data; 
+		// console.log(utf8message);
 		// treatment.splitFrame(utf8message);
-		});
+		// });
 
-    function sendRequest(rqst) {
-        if (connection.connected) {
-            connection.send(JSON.stringify(rqst));
-			console.log(JSON.stringify(rqst));	
-        }
-    }
+    // function sendRequest(rqst) {
+        // if (connection.connected) {
+            // connection.send(JSON.stringify(rqst));
+			// console.log(JSON.stringify(rqst));	
+        // }
+    // }
     // sendRequest(rqstTicker);
-	sendRequest(rqstTicker1);
+	// sendRequest(rqstTicker1);
+	// sendRequest(rqstTicker1);
+	// sendRequest(rqstTicker1);
 	// sendRequest(rqstAuth);
 	// sendRequest(rqstReport);
-});
-client.connect('wss://api.hitbtc.com/api/2/ws');                                                                          
+// });
+// client.connect('wss://api.hitbtc.com/api/2/ws');    
+
+
+
+// -------
+
+
+
+websocket = new WebSocket("wss://api.hitbtc.com/api/2/ws");
+
+websocket.onopen = function() {
+  console.log("<p>> CONNECTED</p>");
+};
+
+websocket.onmessage = function(evt) {
+  console.log("<p style='color: blue;'>> RESPONSE: " + evt.data + "</p>");
+};
+
+websocket.onerror = function(evt) {
+  console.log("<p style='color: red;'>> ERROR: " + evt.data + "</p>");
+};
+}
+
+function sendMessage(message) {
+console.log("<p>> SENT: " + message + "</p>");
+websocket.send(message);
+}
+
+sendMessage (rqstTicker1);
+
+
+
+                                                                      
