3bec4c3db0d19685c0d623396a171dde78b21620
atouloupis <atouloupis@yahoo.Fr>
2017-11-28 15:01:39 +0100

    Add files via upload

--- websocket/checkOrder.js
+++ websocket/checkOrder.js
@@ -3,10 +3,10 @@ module.exports.hasAnOrder = hasAnOrder;
 function hasAnOrder (tickerFrame) {
 	var activeOrders = require('../data/activeOrders.json');
 	var k = 0;
-	
+	console.log("#"+JSON.stringify(tickerFrame);
 	for (var j=0;j<activeOrders.length;j++)
 	{
-		// console.log("##"+JSON.stringify(activeOrders[j]));
+		 console.log("##"+JSON.stringify(activeOrders[j]));
 		if (activeOrders[j].symbol == tickerFrame.params.symbol)
 			{
 			activeSellOrBuy(activeOrders[j],tickerFrame);
@@ -14,14 +14,14 @@ function hasAnOrder (tickerFrame) {
 			// console.log("###"+JSON.stringify(tickerFrame));
 			}		
 	}
-	if (k=0) callEligibility (tickerFrame);
+	if (k==0) callEligibility (tickerFrame);
 
 	
 }	
 
 function activeSellOrBuy (order,ticker)
 	{
-	console.log("####"+JSON.stringify(ticker));
+	// console.log("####"+JSON.stringify(ticker));
 	}
 	
 function callEligibility (ticker)
