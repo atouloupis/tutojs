73a42bb1240a32d188fe943134df6f84d7fcc7a7
atouloupis <atouloupis@yahoo.Fr>
2017-11-29 12:48:50 +0100

    Add files via upload

--- websocket/wsClient.js
+++ websocket/wsClient.js
@@ -81,7 +81,7 @@ var connected = false;
 // });
 
 var io = require('socket.io-client');
-var socket = io.connect('ws://demos.kaazing.com/echo', {reconnect: true,origins:"*"});
+var socket = io.connect('ws://demos.kaazing.com/echo', {reconnect: true,transports: ['websocket'],origins:"*"});
 
 
 socket.on('connect', function(){
