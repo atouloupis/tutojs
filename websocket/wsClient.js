9134a7d0746104b95d434aff97e08da902b2cc6c
atouloupis <atouloupis@yahoo.Fr>
2017-11-30 18:04:50 +0100

    Add files via upload

--- websocket/wsClient.js
+++ websocket/wsClient.js
@@ -5,7 +5,7 @@ var jsonfile = require('jsonfile');
 var treatment = require('./treatmentFrame');
 
 var rqstTicker = {   "method": "subscribeTicker",   "params": {     "symbol": "IXTETH"   },   "id": 123 }; 
-var rqstTicker1 = {   "method": "subscribeTicker",   "params": {     "symbol": "BQXETH"   },   "id": 123 }; 
+var rqstTicker1 = {   "method": "subscribeTicker",   "params": {     "symbol": "ETHBTC"   },   "id": 123 }; 
 var rqstAuth = {   "method": "login",   "params": {     "algo": "BASIC",     "pKey": "75ea4dceeb285ee86c026d62700df14f",     "sKey": "919c86f1a996fa98ba4fc74ebb1a364d"   } };
 var rqstReport = {   "method": "subscribeReports",   "params": {} }; 
 var file = 'data.json';
@@ -57,7 +57,7 @@ var ws = new WebSocket("wss://api.hitbtc.com/api/2/ws");
 var connected = false;
 ws.onopen = function() {
   console.log("<p>> CONNECTED</p>");
-  var connected = true;
+  connected = true;
 };
 
 ws.onmessage = function(evt) {
@@ -69,6 +69,7 @@ ws.onerror = function(evt) {
 };
 
 function sendMessage(message) {
+console.log("Inside SendMessage");
 if (connected==true) {
 console.log("<p>> SENT: " + message + "</p>");
 ws.send(message);
