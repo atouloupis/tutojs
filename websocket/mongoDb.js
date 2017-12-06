d9e633534a1d85ada9215ebfcedd7473c157c091
atouloupis <atouloupis@yahoo.Fr>
2017-12-06 16:45:36 +0100

    Add files via upload

--- websocket/mongoDb.js
+++ websocket/mongoDb.js
@@ -40,7 +40,8 @@ var url = "mongodb://localhost:27017/"+dbName;
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  db.collection(collectionName).insertOne(myObj, function(err, res) {
+  var dbase = db.db(dbName);
+  dbase.collection(collectionName).insertOne(myObj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
     db.close();
