13c41efff1233d1090c3803caee6613856812408
atouloupis <atouloupis@yahoo.Fr>
2017-12-06 14:24:01 +0100

    Add files via upload

---
+++ websocket/MongoDb.js
@@ -0,0 +1,63 @@
+module.exports.createMongoDb = createMongoDb;
+module.exports.createMongoCollection = createMongoCollection;
+module.exports.insertMongoCollection = insertMongoCollection;
+// module.exports.createMongoDb = createMongoDb;
+
+var mongo = require('mongodb');
+
+function createMongoDb(dbName)
+{
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/"+dbName;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+}
+
+function createMongoCollection(dbName,collectionName)
+{
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/"+dbName;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.createCollection(collectionName, function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
+}
+
+function insertMongoCollection(dbName,collectionName,myObj)
+{
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/"+dbName;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.collection(collectionName).insertOne(myObj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+}
+
+// function createMongoDb(name)
+// {
+
+// }
+
+// function createMongoDb(name)
+// {
+
+// }
+
+// function createMongoDb(name)
+// {
+
+// }
--- websocket/orderBook.js
+++ websocket/orderBook.js
@@ -1,6 +1,10 @@
+var MongoDb = require ('./MongoDb');
+
+
 module.exports.updateOrderBook = updateOrderBook;
 
 function updateOrderBook(orderBookFrame)
 {
+mongoDb.createMongoCollection("orderBook",orderBookFrame);
 console.log("#"+JSON.stringify(orderBookFrame));
 }
