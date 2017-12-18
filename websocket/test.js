eca95d1836795244acb5d85c18d34c9984ec8536
atouloupis <atouloupis@yahoo.Fr>
2017-12-18 20:47:55 +0100

    Add files via upload

--- websocket/test.js
+++ websocket/test.js
@@ -6,8 +6,8 @@ var collectionName = "orderBookFrame";
 //mongoDb.deleteRecords(dbName,collectionName,JSON.parse('{ "symbol" : "BTGETH" }'),function(){});
 var objAdd=JSON.parse('{ "symbol" : "BTGETH", "way" : "ask", "params" : {"size":10, "price" : 123456} }')
 //mongoDb.insertCollection(dbName,collectionName,objAdd,function(){});
-var o_id = new mongo.ObjectID("5a3814a2e2a0622ed39b8d48");
-var updateQuery= JSON.parse('{ "_id" :"' + o_id + '"}');
+var updateQuery = { _id: new mongo.ObjectID("5a3814a2e2a0622ed39b8d48")};
+//var updateQuery= JSON.parse('{ "_id" :"' + o_id + '"}');
 var newValues =JSON.parse('{"$set": {"params" : { "size" : 20}}}');
 
 mongoDb.findRecords(dbName,collectionName,updateQuery,function(message){
