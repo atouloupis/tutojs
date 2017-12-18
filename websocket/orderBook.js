2c71c442cd7b1d0d2f4a6b9f10be7ea4de78e071
atouloupis <atouloupis@yahoo.Fr>
2017-12-18 16:02:09 +0100

    Add files via upload

--- websocket/orderBook.js
+++ websocket/orderBook.js
@@ -113,9 +113,9 @@ for (var i=0;i<1;i++)
 			}
 mongoDb.findRecords(dbName,collectionName,"",function(message){
 	//console.log(message);
-	console.log("to emit");
+	
 	message = JSON.stringify(message);
-	io.emit(message);
+	io.emit('chat message',message);
 		//socket.broadcast.emit('message',message);
 
 });
