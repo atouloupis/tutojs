fb41b48fe8bf086942b204f340bc7537db494b99
atouloupis <atouloupis@yahoo.Fr>
2017-12-18 20:13:47 +0100

    Add files via upload

--- websocket/test.js
+++ websocket/test.js
@@ -2,4 +2,4 @@ var mongoDb = require ('./mongoDb');
 var dbName = "orderBook";
 var collectionName = "orderBookFrame";
 
-mongoDb.deleteRecords(dbName,collectionName,'{ "symbol" : "BTGETH" }',function(){});
+mongoDb.deleteRecords(dbName,collectionName,JSON.parse('{ "symbol" : "BTGETH" }'),function(){});
