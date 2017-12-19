dcabd7de0d30c749592bc835ffbe3af468945a51
atouloupis <atouloupis@yahoo.Fr>
2017-12-19 18:36:33 +0100

    Add files via upload

--- websocket/orderBook.js
+++ websocket/orderBook.js
@@ -18,78 +18,99 @@ module.exports.updateOrderBook = updateOrderBook;
 
 function updateOrderBook(orderBookFrame, method,callback)
 {
-var dbName = "orderBook";
-var collectionName = "orderBookFrame";
-var symbol = orderBookFrame.symbol;
-// Cr�er la collection
-count(25);
-mongoDb.createCollection(dbName,collectionName,function(){
-//console.log(orderBookFrame);
-//Si methode = snapshotOrderbook, supprime et remplace toutes les valeurs pour ce symbol
-if (method=="snapshotOrderbook")
-	{
-	count(31);
-	//console.log(orderBookFrame.symbol);
-	deleteQuery = JSON.parse('{ "symbol" : "'+symbol+'" }');
-	//console.log(deleteQuery);
-	mongoDb.deleteRecords(dbName,collectionName,deleteQuery,function(){
-	count(36);
-	//console.log("deleted");
-	//D�couper la trame pour respecter format
-	//D�coupe de ask et enregistrement
-	var orderBookAskArray=orderBookFrame.ask;
-	//console.log("Length :" + orderBookFrame.ask.length);
+	var dbName = "orderBook";
+	var collectionName = "orderBookFrame";
+	var symbol = orderBookFrame.symbol;
+	// Cr�er la collection
+	count(25);
+	mongoDb.createCollection(dbName,collectionName,function(){
+		//Si methode = snapshotOrderbook, supprime et remplace toutes les valeurs pour ce symbol
+		if (method=="snapshotOrderbook")
+		{
+			count(31);
+			deleteQuery = JSON.parse('{ "symbol" : "'+symbol+'" }');
+			mongoDb.deleteRecords(dbName,collectionName,deleteQuery,function(){
+				count(36);
+				//D�couper la trame pour respecter format
+				//D�coupe de ask et enregistrement
+				var orderBookAskArray=orderBookFrame.ask;	
+				//Appel de la fonction d'ajout des ASK à partir d'un snapshot
+				snapshotAddAsk(orderBookAskArray,function(){});
+				//D�coupe de bid et enregistrement
+				var orderBookBidArray=orderBookFrame.bid;
+				//Appel de la fonction d'ajout des BID à partir d'un snapshot
+				snapshotAddBid(orderBookBidArray,function(){});
+			});
+		}	
+		else {
+			// R�cup�rer donn�es dans Mongo
+			var findSymbolRecords = JSON.parse('[{ "symbol" : "'+symbol+'", "way" : "bid"},{ "symbol" : "'+symbol+'", "way" : "ask"}]');
+			/////////////////////////////Pour les Bid/ask ////////////////
+			for (var k=0;k<1;k++)
+			{
+				// Delete doublons 
+				deleteDouble(findSymbolRecords[k],function (){
+					insertOrReplace(findSymbolRecords[k],function(){});
+				});
+			}
+		}
+	});
+	mongoDb.findRecords(dbName,collectionName,"",function(message){
+		console.log(message.length);
+		var bid = [];
+		var ask =[];
+		for (var i=0;i<message.length;i++)
+		{
+			if (message[i].way == "bid")
+				{bid.push(message[i].params.price);}
+			else 
+				{ask.push(message[i].params.price);}
+		}
+		io.emit('bid message',bid.toString());
+		io.emit('ask message', ask.toString());
+
+	});
+	function count(line){
+		mongoDb.count(dbName,collectionName,function(count){
+			console.log(count+"ligne : "+line);
+		});
+	}
+
+function snapshotAddAsk(orderBookAskArray,callback) {
 	for (var i=0;i<orderBookAskArray.length;i++)
 		{
-		//console.log("i = " + i);
 		var askPriceSize=JSON.stringify(orderBookAskArray[i]);
-		var objAdd = JSON.parse('{ "symbol" : "'+symbol+'", "way" : "ask", "params" : ' + askPriceSize +' }');
-		//console.log(objAdd);
+		var objAdd = JSON.parse('{ "symbol" : "'+symbol+'", "way" : "ask", "params" : ' + askPriceSize +' }');;
 		count(48);
-		mongoDb.insertCollection(dbName,collectionName,objAdd,function(){});
+		mongoDb.insertCollection(dbName,collectionName,objAdd,function(){
 		count(50);
+		if (i==orderBookAskArray.length-1)callback();
+		});
 		}
-	//D�coupe de bid et enregistrement
-	var orderBookBidArray=orderBookFrame.bid;
-	for (var i=0;i<orderBookFrame.bid.length;i++)
+}
+function snapshotAddBid(orderBookBidArray,callback) {
+	for (var i=0;i<orderBookBidArray.length;i++)
 		{
-		// console.log("i = " + i);
 		var bidPriceSize=JSON.stringify(orderBookBidArray[i]);
 		var objAdd = JSON.parse('{ "symbol" : "'+symbol+'", "way" : "bid", "params" : ' + bidPriceSize +' }');
-		//console.log(objAdd);
 		count(60);
 		mongoDb.insertCollection(dbName,collectionName,objAdd,function(){
 		count(62);
-		if (i==orderBookFrame.bid.length-1)callback("terminé1");
+		if (i==orderBookBidArray.length-1)callback();
 		});
 		}
-	});
-	}	
-else {
-// R�cup�rer donn�es dans Mongo
-	var findSymbolRecords = JSON.parse('[{ "symbol" : "'+symbol+'", "way" : "bid"},{ "symbol" : "'+symbol+'", "way" : "ask"}]');
-	// console.log("LENGTH :" +findSymbolRecords.length);
-	 // console.log("LENGTH :" +JSON.stringify(findSymbolRecords[0]));
-	 // console.log("LENGTH :" +JSON.stringify(findSymbolRecords[1]));
-/////////////////////////////Pour les Bid/ask ////////////////
-for (var k=0;k<1;k++)
-	{
-	mongoDb.findRecords(dbName,collectionName,findSymbolRecords[k],function(symbolRecords){
-		// Delete doublons 
+}
+
+function deleteDouble(findSymbolRecords,callback){
+	mongoDb.findRecords(dbName,collectionName,findSymbolRecords,function(symbolRecords){
 		var deleteQuery=[];
 		for (var i=0;i<symbolRecords.length;i++)
 			{
 			for (var j=i+1;j<symbolRecords.length;j++)
 				{
-				//console.log(symbolRecords[i].params.price+" = "+symbolRecords[j].params.price);
-				
 				if(symbolRecords[i].params.price == symbolRecords[j].params.price)
 					{
-					console.log(symbolRecords[i].params.price+" = "+symbolRecords[j].params.price);
-					// console.log(symbolRecords[j]);
-					// console.log(symbolRecords[i]);
 					 deleteQuery = [deletequery,'{ "symbol" : "'+symbol+'", "_id" : "' + symbolRecords[j]._id + '" }'];
-					 //console.log("doublon delete" + deleteQuery);
 					}
 				}
 			}
@@ -97,18 +118,18 @@ for (var k=0;k<1;k++)
 		{
 			count(91);
 			mongoDb.deleteRecords(dbName,collectionName,JSON.parse(deleteQuery[i]),function(){
-			//console.log("DELETED line 84");
+			if (i==deleteQuery.length-1)callback();
 			count(94);
 			});
 		}
-	
-	mongoDb.findRecords(dbName,collectionName,findSymbolRecords[k],function(symbolRecords){
+	});
+}
+
+function insertOrReplace(findSymbolRecords,callback){
+	mongoDb.findRecords(dbName,collectionName,findSymbolRecords,function(symbolRecords){
 		for (var i=0;i<symbolRecords.length;i++)
 		{
 			// Chercher si prix existe d�j�	
-			//console.log(orderBookFrame);
-			//console.log(symbolRecords[i].params.price);
-			// console.log("This is the TYPEOF" + typeof orderBookFrame.bid[0]);
 			if (typeof orderBookFrame.bid[0] != "undefined")
 				{
 				var orderBookFrameBidPrice=orderBookFrame.bid[0].price;
@@ -117,64 +138,29 @@ for (var k=0;k<1;k++)
 					{
 					// si oui remplacer size
 					var newValues = JSON.parse('{ "$set": {"params" : { "size" : '+orderBookFrame.bid[0].size+'}}}');
-					//console.log(newValues);
 					var updateQuery = { _id: new mongo.ObjectID(symbolRecords[i]._id)};
-					//console.log("UPDATE QUERY");
-					 //console.log(updateQuery);
 					 count(114);
 					mongoDb.updateCollection(dbName,collectionName,updateQuery, newValues,function(){
 					count(116);
-					callback("terminé2");
-					//console.log("updated");
+					if (i==symbolRecords.length-1)callback();
 					});
 					}
 				// si non cr�er une nouvelle entr�e
 				else 
 					{
 					var newEntryQuery = JSON.parse('{ "symbol" : "'+symbol+'", "way" : "bid", "params" : { "price" : "'+orderBookFrameBidPrice+'", "size" : "'+orderBookFrameBidSize+'"}}');
-					//console.log("newEntryQuery");
 					count(125);
 					mongoDb.insertCollection(dbName,collectionName,newEntryQuery,function(){
 					count(127);
-					callback("terminé3");
-					//console.log("newEntryOK");
+					if (i==symbolRecords.length-1)callback();
 					});
 					}
 				}
-				else callback("terminé4");
+				else callback();
 			}
 		});
-		});
-	}
-
-	}
-	
-
-});
-mongoDb.findRecords(dbName,collectionName,"",function(message){
-	console.log(message.length);
-	var bid = [];
-	var ask =[];
-	//message = JSON.stringify(message);
-	for (var i=0;i<message.length;i++)
-	{
-		if (message[i].way == "bid")
-		{bid.push(message[i].params.price);}
-		else 
-		{ask.push(message[i].params.price);}
-	}
-	//console.log(bid);
-	io.emit('bid message',bid.toString());
-	io.emit('ask message', ask.toString());
-		//socket.broadcast.emit('message',message);
-
-});
-function count(line){
-
-mongoDb.count(dbName,collectionName,function(count){
-	console.log(count+"ligne : "+line);
-	});
 }
+
 }
 
 
