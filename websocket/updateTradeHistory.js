628497c022f1849dbcc167ea11137a171c9fa485
atouloupis <atouloupis@yahoo.Fr>
2018-01-03 17:16:40 +0100

    Add files via upload

--- websocket/updateTradeHistory.js
+++ websocket/updateTradeHistory.js
@@ -7,12 +7,17 @@ function newTradeHistory(frame) {
 				// console.log("NewTradeHistory");
 			// console.log(frame);
         for (var i = 0; i < frame.data.length; i++) {
-		
-            var queryUpdate = {igrd:frame.data[i].id, price:frame.data[i].price, quantity:frame.data[i].quantity, side:frame.data[i].side, timestamp:frame.data[i].timestamp, symbol:frame.symbol};
-			// console.log("NewTradeHistory");
-			// console.log(queryUpdate);
-            // mongoDb.updateCollection(collectionName, queryUpdate, queryUpdate, function () {
-            // });
+		var id=frame.data[i].id;
+		var price=frame.data[i].price;
+		var quantity=frame.data[i].quantity;
+		var side=frame.data[i].side;
+		var timestamp=frame.data[i].timestamp;
+		var symbol=frame.symbol;
+            var queryUpdate = {"id":id, "price":price, "quantity":quantity, "side":side, "timestamp":timestamp, "symbol":symbol};
+			//console.log("NewTradeHistory");
+			console.log(queryUpdate);
+            mongoDb.updateCollection(collectionName, queryUpdate, queryUpdate, function () {
+            });
         }
     });
 }
