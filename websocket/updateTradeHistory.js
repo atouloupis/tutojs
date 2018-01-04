7b7b821ac1e7aabae013f820b6299c39fdca54cd
atouloupis <atouloupis@yahoo.Fr>
2018-01-04 17:23:57 +0100

    Add files via upload

--- websocket/updateTradeHistory.js
+++ websocket/updateTradeHistory.js
@@ -13,8 +13,10 @@ function newTradeHistory(frame) {
 		var side=frame.data[i].side;
 		var timestamp=frame.data[i].timestamp;
 		var symbol=frame.symbol;
-            var queryUpdate = JSON.parse('{"id":"'+id+'", "price":"'+price+'", "quantity":"'+quantity+'", "side":"'+side+'", "timestamp":"'+timestamp+'", "symbol":"'+symbol+'"}');
-			//console.log("NewTradeHistory");
+            var queryUpdate = {id:id, price:price, quantity:quantity, side:side, timestamp:timestamp, symbol:symbol});
+			// db.tradeHistory.update({ id: '132614043', price: '1.292738', quantity: '0.01', side: 'sell', timestamp: '2018-01-03T14:23:44.623Z', s
+// ymbol: 'DASHETH' },{ id: '132614043', price: '1.292738', quantity: '0.01', side: 'sell', timestamp: '2018-01-03T14:23:44.623Z', symbol:
+ // 'DASHETH' },{upsert:true})
 			console.log(queryUpdate);
             mongoDb.updateCollection(collectionName, queryUpdate, queryUpdate, function () {
             });
