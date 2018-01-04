0ad2b6036c37aa0fe758e9f0a87ee573b2eca3d3
atouloupis <atouloupis@yahoo.Fr>
2018-01-04 17:31:01 +0100

    Add files via upload

--- websocket/test.js
+++ websocket/test.js
@@ -1,7 +1,8 @@
-// var mongo = require('mongodb');
 // var mongoDb = require ('./mongoDb');
-// var dbName = "orderBook";
-// var collectionName = "orderBookFrame";
+var dbName = "orderBook";
+var collectionName = "history";
+var mongoClient = require('mongodb').MongoClient;
+var urlOrderBook = "mongodb://localhost:27017/orderBook";
 
 //mongoDb.deleteRecords(dbName,collectionName,JSON.parse('{ "symbol" : "BTGETH" }'),function(){});
 // var objAdd=JSON.parse('{ "symbol" : "BTGETH", "way" : "ask", "params" : {"size":10, "price" : 123456} }')
@@ -29,7 +30,7 @@
 //db.orderBookFrame.updateOne({symbol:"IXTETH",way:"ask","params.price":"0.000679"},{$set:{symbol: "IXTETH",way: "ask",params: {price: "0.000679",size: 100}}}
 
 
-//db.orderBookFrame.updateOne({ symbol: 'BCHETH', way: 'ask', 'params.price': '3.895362' },{'$set':{ symbol: 'BCHETH',way: 'ask', params:{ price: '3.895362', size: '0.00' } } })
+// db.orderBookFrame.updateOne({ symbol: 'BCHETH', way: 'ask', 'params.price': '3.895362' },{'$set':{ symbol: 'BCHETH',way: 'ask', params:{ price: '3.895362', size: '0.00' } } })
 
 //db.orderBookFrame.aggregate([{ $group: {_id: { firstField: "$price", secondField: "$size" },uniqueIds: { $addToSet: "$_id" },count: { $sum: 1 }}},{ $match: {count: { $gt: 1 }}}])
 //db.orderBookFrame.aggregate([{ $group: {_id: "$params.price",oderBookFrame: { $push: "$params.size" },count: { $sum: 1 }}},{ $match: {count: { $gt: 1 }}}])
@@ -37,3 +38,12 @@
 var date = new Date;
 
 console.log(date.getHours());
+
+
+mongoClient.connect(urlOrderBook, function (err, db) {
+    if (err) throw err;
+    var dbOrderBook = db.db("orderBook");
+	
+	dbOrderBook.collectionName.updateOne({ symbol: 'BCHETH', way: 'ask', 'params.price': '3.895362' },{'$set':{ symbol: 'BCHETH',way: 'ask', params:{ price: '3.895362', size: '0.00' } } });
+	
+	});
