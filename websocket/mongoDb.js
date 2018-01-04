caf31b8b5022f00cdc581301ebb5fdd8577c3b0e
atouloupis <atouloupis@yahoo.Fr>
2018-01-04 17:17:25 +0100

    Add files via upload

--- websocket/mongoDb.js
+++ websocket/mongoDb.js
@@ -5,7 +5,6 @@ module.exports.findRecords = find;
 module.exports.updateCollection = update;
 module.exports.count = count;
 
-var mongo = require('mongodb');
 var connectDbaseSource = require('./wsClient.js');
 
 function createMongoCollection(collectionName, callback) {
@@ -51,7 +50,8 @@ function find(collectionName, query, callback) {
 
 
 function update(collectionName, query, newValues, callback) {
-	connectDbaseSource.dbase.collection(collectionName).updateOne(query, newValues, {upsert:true}, function(err, res) {
+	
+	connectDbaseSource.dbase.collection(collectionName).updateMany(query, newValues, {upsert:true}, function(err, res) {
         if (err) throw err;
         callback();
     });
