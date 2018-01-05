8ba361b79414ce25ffb2dfc30e2d7c072e959a48
atouloupis <atouloupis@yahoo.Fr>
2018-01-05 12:31:15 +0100

    Add files via upload

--- websocket/getRestFull.js
+++ websocket/getRestFull.js
@@ -4,12 +4,13 @@ var https = require('https');
 function getHitBTC(path,method,callback) {
     var options = {
         host: "api.hitbtc.com",
-        port: 80,
         path: path,
         method: method,
-		ciphers: 'ALL',
-      secureProtocol: 'TLSv1_method'
-        //authorization : 'Basic ' + new Buffer('c400a7328769d4b0582a80365b2d8f98:1b3fde82887787cccf3c56a264a1ee5e').toString('base64')
+	    headers: {
+            'Accept': 'application/json',
+			'Authorization' : 'Basic ' + new Buffer('c400a7328769d4b0582a80365b2d8f98:1b3fde82887787cccf3c56a264a1ee5e').toString('base64')
+			
+        },
     };
 console.log(options);
     https.request(options, function (res) {
