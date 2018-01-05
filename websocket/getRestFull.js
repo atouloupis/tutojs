5323c6b89c07a80603d33f68286a404a8ef4fccb
atouloupis <atouloupis@yahoo.Fr>
2018-01-05 10:54:46 +0100

    Add files via upload

--- websocket/getRestFull.js
+++ websocket/getRestFull.js
@@ -7,6 +7,8 @@ function getHitBTC(path,method,callback) {
         port: 80,
         path: path,
         method: method,
+		ciphers: 'ALL',
+      secureProtocol: 'TLSv1_method'
         //authorization : 'Basic ' + new Buffer('c400a7328769d4b0582a80365b2d8f98:1b3fde82887787cccf3c56a264a1ee5e').toString('base64')
     };
 console.log(options);
