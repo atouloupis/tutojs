4c1483f464a544140b8a9987d407e5cd03c72b79
atouloupis <atouloupis@yahoo.Fr>
2018-01-05 15:48:48 +0100

    Add files via upload

--- websocket/wsClient.js
+++ websocket/wsClient.js
@@ -109,11 +109,10 @@ mongoClient.connect(urlOrderBook, function (err, db) {
 		var collectionName = "symbol";
     mongoDb.createCollection(collectionName, function () {
 		            api.getHitBTC("/api/2/public/symbol","GET", function (symbol) {
-					var query = [];
+					var query = toArray(symbol);
 					console.log(symbol);
-					for (var i=0;i<symbol.length;i++)query[i] = symbol[i];
                 mongoDb.deleteRecords(collectionName, {}, function () {
-				//console.log(query);
+				console.log(query);
                     mongoDb.insertCollection(collectionName, query, function () {
                     })
                 });
