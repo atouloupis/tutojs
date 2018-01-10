74bcefb8638331ca13af5f36b05dbe1f569a0232
atouloupis <atouloupis@yahoo.Fr>
2018-01-10 10:47:23 +0100

    Add files via upload

--- websocket/eligibility.js
+++ websocket/eligibility.js
@@ -77,13 +77,13 @@ function buy (ticker,callback) {
 		else {
 			//console.log(tradingBalance.length);
 		for (var i = 0; i < tradingBalance.length; i++) {
-		console.log(toString(ticker.symbol));
-		console.log(toString(ticker.symbol).length);
-		console.log(toString(ticker.symbol).length-3);
-		console.log("ETHBTC".substr(0, toString(ticker.symbol).length - 3))
-		console.log(toString(ticker.symbol).substr(0, toString(ticker.symbol).length - 3));
+		console.log(JSON.stringify(ticker.symbol));
+		console.log(JSON.stringify(ticker.symbol).length);
+		console.log(JSON.stringify(ticker.symbol).length-3);
+		console.log("ETHBTC".substr(0, JSON.stringify(ticker.symbol).length - 3))
+		console.log(JSON.stringify(ticker.symbol).substr(0, JSON.stringify(ticker.symbol).length - 3));
 		//console.log(tradingBalance[i].currency);
-            if (tradingBalance[i].currency == toString(ticker.symbol).substr(0, toString(ticker.symbol).length - 3)) 
+            if (JSON.stringify(tradingBalance[i].currency) == JSON.stringify(ticker.symbol).substr(0, JSON.stringify(ticker.symbol).length - 3)) 
 			{ balanceAvailable = tradingBalance[i].available;
 			console.log("balance available : "+balanceAvailable);
 			
