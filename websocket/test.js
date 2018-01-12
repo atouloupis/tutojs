9343131e0d6c43cb6d916817e4ecf0f192cc4551
atouloupis <atouloupis@yahoo.Fr>
2018-01-12 13:32:49 +0100

    Add files via upload

--- websocket/test.js
+++ websocket/test.js
@@ -8,26 +8,25 @@ mongoClient.connect(url, function(err, db) {
     if (err) throw err;
 	var date2 = new Date;
     var connectDbaseSource = db.db("heavy");
-	var myObj=[];
-    for (var j = 0; j < 960000; j++) {
-	myObj.push({id:j});
-	}
+	// var myObj=[];
+    // for (var j = 0; j < 960000; j++) {
+	// myObj.push({id:j});
+	// }
 
-	connectDbaseSource.collection(collectionName).insertMany(myObj, function(err, res) {
-        if (err) throw err;
-		console.log("Number of documents ="+ res.length)
-		var date1 = new Date;
-		var time = date1-date2;
-		console.log("time ="+time+" ms");
-
-});
-// for (var j = 0; j < 960000; j++) {
-	    // connectDbaseSource.collection(collectionName).find("").toArray(function(err, result) {
+	// connectDbaseSource.collection(collectionName).insertMany(myObj, function(err, res) {
         // if (err) throw err;
-		// console.log("Number of documents ="+ result.length)
 		// var date1 = new Date;
 		// var time = date1-date2;
 		// console.log("time ="+time+" ms");
-    // });
-    // }
+
+// });
+for (var j = 0; j < 1; j++) {
+	    connectDbaseSource.collection(collectionName).find("").toArray(function(err, result) {
+        if (err) throw err;
+		console.log("Number of documents ="+ result.length)
+		var date1 = new Date;
+		var time = date1-date2;
+		console.log("time ="+time+" ms");
+    });
+    }
 });
